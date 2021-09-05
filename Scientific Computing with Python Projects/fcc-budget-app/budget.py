4370940df676ac04ebc0101ec839618c9605d44f
Keanu <k.h.dirkse78@gmail.com>
2021-09-05 12:53:15 +0200

    adding empty functions

--- Scientific Computing with Python Projects/fcc-budget-app/budget.py
+++ Scientific Computing with Python Projects/fcc-budget-app/budget.py
@@ -1,7 +1,13 @@
 class Category:
-
-
-
-
-
-def create_spend_chart(categories):
+    def create_spend_chart(self,categories):
+        pass
+    def deposit(self,amount,description):
+        pass
+    def withdraw(self,amount,description):
+        pass
+    def get_balance(self):
+        pass
+    def transfer(self,amount,category):
+        pass
+    def check_funds(self,amount):
+        pass
