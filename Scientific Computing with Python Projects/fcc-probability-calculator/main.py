7f14873e2ba95e5090daba51342416b793d74f61
Keanu Dirkse <k.h.dirkse78@gmail.com>
2021-11-21 22:07:01 +0200

    completing probablity python project

--- Scientific Computing with Python Projects/fcc-probability-calculator/main.py
+++ Scientific Computing with Python Projects/fcc-probability-calculator/main.py
@@ -2,12 +2,12 @@
 import prob_calculator
 from unittest import main
 
-hat = prob_calculator.Hat(blue=4, red=2, green=6)
+hat = prob_calculator.Hat(blue=40, red=1, )
 probability = prob_calculator.experiment(
     hat=hat,
     expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
+                    "red": 10},
+    num_balls_drawn=3,
     num_experiments=3000)
 print("Probability:", probability)
 
--- Scientific Computing with Python Projects/fcc-probability-calculator/prob_calculator.py
+++ Scientific Computing with Python Projects/fcc-probability-calculator/prob_calculator.py
@@ -4,6 +4,64 @@ import random
 
 class Hat:
 
+  def __init__(self,**kwargs):
+    contents=[]
+    key_list = list(kwargs.keys())
+    val_list = list(kwargs.values())
+    for index, key  in enumerate(key_list):
+      for i in range(val_list[index]):
+        contents.append(key)
 
-  def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+
+    self.contents=contents
+
+  def draw(self,number):
     
+    if number > len(self.contents):
+      return self.contents
+
+    results=random.sample(self.contents, number)
+    
+    for result in results:
+      self.contents.remove(result)
+
+    
+    return results
+
+    
+
+
+def matchedDraw(expected_balls,draw):
+
+  key_list = list(expected_balls.keys())
+  val_list = list(expected_balls.values())
+  contents=[]
+  for index, key  in enumerate(key_list):
+      for i in range(val_list[index]):
+        contents.append(key)
+
+  matches=True
+
+  for ball in contents:
+    if( ball in draw):
+      draw.remove(ball)
+      
+    else:
+      matches=False
+
+  return matches
+
+  
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  
+  propabilty=0
+  results=[]
+  count=0
+  for i in range(num_experiments):
+    tempHat=copy.deepcopy(hat)
+    drawn=tempHat.draw(num_balls_drawn)
+    if(matchedDraw(expected_balls,drawn)):
+      count+=1
+
+  return count/num_experiments
+  
